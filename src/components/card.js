ac12ac40ad0090a41cf40b28a8e833cd11a1fca8
Alfredo C <alfredo_cardenas@live.com>
2025-04-01 23:12:47 -0600

    Add the card component and remove the css for components - due to Tailwind being installed and setup in the next commit

---
+++ src/components/card.js
@@ -0,0 +1,14 @@
+import '../styles/components.scss';
+
+const Card = ({ className, children, ...props }) => {
+  return (
+    <div className={`rounded-xl border bg-card
+      text-card-foreground shadow p-6 ${className}`}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+};
+
+export { Card };
